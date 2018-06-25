e6bbd751902dfc9cfaf40c07d6a0920032466850
Nicholas Adamou <nicholasadamouemail@gmail.com>
2018-06-24 22:02:16 -0700

    fixed version issue

--- .dotfiles/tag-my/modules/install.sh
+++ .dotfiles/tag-my/modules/install.sh
@@ -5,6 +5,6 @@
 export SMU_BLUEPRINT=${SMU_BLUEPRINT:-"nicholasadamou/set-me-up-blueprint"}
 
 # The set-me-up version to download
-export SMU_VERSION=${SMU_VERSION:-"1.0.0"}
+export SMU_VERSION=${SMU_VERSION:-"1.0.1"}
 
 bash <(curl --progress-bar -L https://raw.githubusercontent.com/nicholasadamou/set-me-up/master/.dotfiles/tag-smu/modules/install.sh) "$@"
