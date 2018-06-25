d8c30023dffa11c5971a4187f99a9b43b5fae8dd
Nicholas Adamou <nicholasadamouemail@gmail.com>
2018-06-24 19:54:21 -0700

    updated

--- .dotfiles/tag-my/modules/install.sh
+++ .dotfiles/tag-my/modules/install.sh
@@ -2,7 +2,7 @@
 
 # GitHub user/repo value of your set-me-up blueprint (e.g.: nicholasadamou/set-me-up-blueprint).
 # Set this value when the installer should additionally obtain your blueprint.
-export SMU_BLUEPRINT=${SMU_BLUEPRINT:-"omares/set-me-up-blueprint"}
+export SMU_BLUEPRINT=${SMU_BLUEPRINT:-"nicholasadamou/set-me-up-blueprint"}
 
 # The set-me-up version to download
 export SMU_VERSION=${SMU_VERSION:-"1.0.0"}
