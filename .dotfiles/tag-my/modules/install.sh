0d3f93d2eaf7d7232e6c8a07c5138248f1dd30c0
Nicholas Adamou <nicholasadamouemail@gmail.com>
2018-12-20 09:30:51 -0500

    updated SMU_VERSION to 'latest'

--- .dotfiles/tag-my/modules/install.sh
+++ .dotfiles/tag-my/modules/install.sh
@@ -6,6 +6,6 @@ export SMU_BLUEPRINT=${SMU_BLUEPRINT:-"nicholasadamou/set-me-up-blueprint"}
 export SMU_BLUEPRINT_BRANCH=${SMU_BLUEPRINT_BRANCH:-"master"}
 
 # The set-me-up version to download
-export SMU_VERSION=${SMU_VERSION:-"LTS"}
+export SMU_VERSION=${SMU_VERSION:-"latest"}
 
 bash <(curl --progress-bar -L https://raw.githubusercontent.com/nicholasadamou/set-me-up/master/.dotfiles/tag-smu/modules/install.sh) "$@"
