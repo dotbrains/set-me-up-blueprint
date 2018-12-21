f48c5c8ed61549d0a2d8c5c02d1679aa6abec985
Nicholas Adamou <nicholasadamouemail@gmail.com>
2018-12-21 11:29:27 -0500

    Staging (#6)

--- .dotfiles/tag-my/modules/install.sh
+++ .dotfiles/tag-my/modules/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# GitHub user/repo value of your set-me-up blueprint (e.g.: nicholasadamou/set-me-up-blueprint).
+# GitHub user/repo & branch value of your set-me-up blueprint (e.g.: nicholasadamou/set-me-up-blueprint/master).
 # Set this value when the installer should additionally obtain your blueprint.
 export SMU_BLUEPRINT=${SMU_BLUEPRINT:-"nicholasadamou/set-me-up-blueprint"}
 export SMU_BLUEPRINT_BRANCH=${SMU_BLUEPRINT_BRANCH:-"master"}
 
 # The set-me-up version to download
-export SMU_VERSION=${SMU_VERSION:-"latest"}
+export SMU_VERSION=${SMU_VERSION:-"master"}
 
 bash <(curl --progress-bar -L https://raw.githubusercontent.com/nicholasadamou/set-me-up/master/.dotfiles/tag-smu/modules/install.sh) "$@"
