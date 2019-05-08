729ab6219f9f1c8ed5261a491f9a78c6009f93e0
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-05-08 05:43:09 -0500

    Update install.sh

--- .dotfiles/modules/install.sh
+++ .dotfiles/modules/install.sh
@@ -6,6 +6,14 @@ export SMU_BLUEPRINT=${SMU_BLUEPRINT:-"nicholasadamou/set-me-up-blueprint"}
 export SMU_BLUEPRINT_BRANCH=${SMU_BLUEPRINT_BRANCH:-"master"}
 
 # The set-me-up version to download
+# Available versions:
+# 1. 'master' (MacOS)
+# 2. 'debian'
 export SMU_VERSION=${SMU_VERSION:-"master"}
 
-bash <(curl -s -L https://raw.githubusercontent.com/nicholasadamou/set-me-up/master/.dotfiles/modules/install.sh) "$@"
+# A set of ignored paths that 'git' will ignore
+# syntax: '<path>|<path>'
+# Note: <path> is relative to '$HOME/set-me-up'
+export SMU_IGNORED_PATHS="${SMU_IGNORED_PATHS:-""}"
+
+bash <(curl -s -L https://raw.githubusercontent.com/nicholasadamou/set-me-up/"${SMU_VERSION}"/.dotfiles/modules/install.sh)
