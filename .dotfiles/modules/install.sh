39c63cc261697fd8d1771a853c3b552bcfbf2ecf
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-01-04 09:04:38 -0500

    update install.sh

--- .dotfiles/modules/install.sh
+++ .dotfiles/modules/install.sh
@@ -7,8 +7,7 @@ export SMU_BLUEPRINT_BRANCH=${SMU_BLUEPRINT_BRANCH:-"master"}
 
 # The set-me-up version to download
 # Available versions:
-# 1. 'master' (MacOS)
-# 2. 'debian'
+# 1. 'master' (default) - Supports macOS, Debian Linux, and Windows (via WSL if the base subsystem is Debian/Ubuntu Linux).
 export SMU_VERSION=${SMU_VERSION:-"master"}
 
 # A set of ignored paths that 'git' will ignore
